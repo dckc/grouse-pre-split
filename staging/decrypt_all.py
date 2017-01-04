6bcfe7e4cbd501a435606d1a11f2908d074fc3d8
Dan Connolly <dconnolly@kumc.edu>
2017-01-04 17:51:58 -0600

    refactor I/O around pathlib API
      - mix_path_utils() extends Path with chmod and decrypt
        - subsumes Decrypt, Fops classes
        - implement chk_path in pathjoin
      - implement walk() work-alike in terms of pathlib
      - main() takes just arg_path;
        I considered passing argv, environ, Path, chmod, etc. to main(),
        but doing '--dry-run' in main() got silly
      - don't import os.path.join outside _tcb():
        it's not a pure fuction since it returns different
        values for the same inputs depending on platform
      - add module doctest
        - flesh out MockPath enough to support it
      - why was filter_prefix in _tcb()? it's data, not a capability
        moved to main()

--- staging/decrypt_all.py
+++ staging/decrypt_all.py
@@ -1,63 +1,92 @@
 ''' Decrypt all CMS files - extracted files are placed in the working directory
+
+Logging is a tested feature:
+
+    >>> from sys import stdout
+    >>> logging.basicConfig(level=logging.INFO, stream=stdout)
+
+    >>> enc_path = MockPath.make_ext('/over/there')
+    >>> main(enc_path)
+    INFO:decrypt_all:Skipped due to filter: f1
+    INFO:decrypt_all:mock_chmod: /over/there/res000050354req_f2, 448
+    INFO:decrypt_all:Decrypting /over/there/res000050354req_f2
+    INFO:decrypt_all:MockPopen::communicate sekret
+    <BLANKLINE>
+    INFO:decrypt_all:Decrypted 1 files
+
 '''
 import stat
-from functools import partial
-from os.path import join as pjoin
 from StringIO import StringIO
+from itertools import tee
 from subprocess import PIPE
 
 import logging  # Exception to OCAP
 
-logging.basicConfig(format='%(asctime)s (%(levelname)s) %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def main(mk_decrypt, mk_fops, get_enc_path, walk, getpass,
-         isfile, abspath, filter_prefix,
+def main(arg_path,
+         filter_prefix='res000050354req',
          decrypt_args=' --overwrite --verbose'):
-    fops = mk_fops()
-    decrypt = mk_decrypt(password=getpass())
+    '''
+    Recursively search for encrypted files matching the filter_prefix pattern.
+
+    @param arg_path: Path to where the delivered HD was copied.
+                     (see mix_path_utils)
+    '''
+    enc_path = arg_path()
 
     decrypt_count = 0
-    for root, dirs, files in walk(get_enc_path()):
-        for name in files:
-            full_path = abspath(pjoin(root, name))
-            if isfile(full_path):
-                if name.startswith(filter_prefix):
-                    fops.chmod(full_path,
-                               stat.S_IXUSR | stat.S_IRUSR | stat.S_IWUSR)
+    for root, dirs, files in walk(enc_path):
+        for f in files:
+            full_path = f.resolve()
+            if full_path.is_file():
+                if f.name.startswith(filter_prefix):
+                    f.chmod(stat.S_IXUSR | stat.S_IRUSR | stat.S_IWUSR)
                     log.info('Decrypting %s' % full_path)
-                    ret = decrypt.decrypt(full_path, decrypt_args)
+                    ret = f.decrypt(decrypt_args)
                     if ret:
                         raise RuntimeError('Return %d from "%s"' %
                                            (ret, full_path))
                     decrypt_count += 1
                 else:
-                    log.info('Skipped due to filter: %s' % name)
+                    log.info('Skipped due to filter: %s' % f.name)
     log.info('Decrypted %d files' % decrypt_count)
 
 
-def mock_do_chmod(path, mode):
-    log.info('chmod: %s, %s' % (path, mode))
+def mix_path_utils(base, popen, password, chmod):
+    class PathRestrict(base):
+
+        def pathjoin(self, other):
+            there = base(str(self)) / other
+            if not there.startswith(str(self)):
+                raise IOError('%s not under %s' % (self, there))
+
+    class PathExt(PathRestrict):
+
+        def chmod(self, mode):
+            chmod(str(self), mode)
+
+        def decrypt(self, decrypt_args=''):
+            proc = popen(str(self) + decrypt_args,
+                         stdin=PIPE, shell=True)
+            proc.communicate(password + '\n')
+            # Warning, possible deadlock if more input is expected
+            return proc.wait()
+
+    return PathExt
 
 
-class Decrypt(object):
-    def __init__(self, popen, password, chk_path):
-        self.password = password
-        self.popen = popen
-        self.chk_path = chk_path
-
-    @classmethod
-    def make(cls, popen, password, chk_path):
-        return Decrypt(popen, password, chk_path)
-
-    def decrypt(self, path, decrypt_args=''):
-        self.chk_path(path)
-        proc = self.popen(path + decrypt_args, stdin=PIPE, shell=True)
-        proc.communicate(self.password + '\n')
-        # Warning, possible deadlock if more input is expected
-        return proc.wait()
+def walk(path):
+    if path.is_dir():
+        root = path
+        dirs, files = tee(root.iterdir())
+        dirs = list(filter(lambda d: d.is_dir(), dirs))
+        files = list(filter(lambda f: f.is_file(), files))
+        yield root, dirs, files
+        for subdir in dirs:
+            for out in walk(subdir):
+                yield out
 
 
 class MockPopen(object):
@@ -73,18 +102,65 @@ class MockPopen(object):
         pass
 
 
-class Fops(object):
-    def __init__(self, chk_path, chmod):
-        self.chk_path = chk_path
-        self.chmod = chmod
+class MockPath(object):
+    fs = {
+        '': {
+            'over': {
+                'there': {
+                    'f1': 'stuff',
+                    'res000050354req_f2': 'junk'}}}}
+
+    def __init__(self, s):
+        from posixpath import join as pathjoin
+        self.pathjoin = lambda other: self.__class__(pathjoin(s, other))
+
+        if not s.startswith('/'):
+            raise NotImplementedError
+        self.resolve = lambda: self
+
+        self._path = s
+        self.name = s.split('/')[-1]
+
+    def __str__(self):
+        return self._path
+
+    def _lookup(self):
+        parts = self._path.split('/')
+        f = self.fs
+        for part in parts:
+            try:
+                f = f[part]
+            except KeyError:
+                raise IOError(part)
+        return f
+
+    def is_dir(self):
+        try:
+            return isinstance(self._lookup(), dict)
+        except IOError:
+            return False
+
+    def is_file(self):
+        try:
+            return isinstance(self._lookup(), str)
+        except IOError:
+            return False
+
+    def iterdir(self):
+        if self.is_dir():
+            for k in sorted(self._lookup().keys()):
+                yield self / k
+        else:
+            raise IOError(self)
+
+    def __div__(self, other):
+        return self.pathjoin(other)
 
     @classmethod
-    def make(cls, chk_path, chmod):
-        return Fops(chk_path, chmod)
-
-    def chmod(self, path, mode):
-        self.chk_path(path)
-        self.chmod(path, mode)
+    def make_ext(cls, path,
+                 password='sekret'):
+        PathClass = mix_path_utils(MockPath, MockPopen, password, mock_chmod)
+        return lambda: PathClass(path)
 
 
 def mock_chmod(path, mode):
@@ -92,33 +168,25 @@ def mock_chmod(path, mode):
 
 
 if __name__ == '__main__':
-    def _tcb(filter_prefix='res000050354req'):
-        from os import walk, chmod, environ
-        from os.path import isfile, abspath
+    def _tcb():
+        from os import chmod, environ
         from subprocess import Popen
         from sys import argv
 
-        # Path to where the delivered HD was copied.  Recursively search
-        # for encrypted files matching the filter_prefix pattern.
-        def get_enc_path():
-            return abspath(argv[1])
+        from pathlib import Path
 
-        def getpass():
-            return environ[argv[2]]
+        (my_popen, my_chmod) = ((MockPopen, mock_chmod)
+                                if '--dry-run' in argv
+                                else (Popen, chmod))
 
-        def chk_path(path):
-            if get_enc_path() not in path:
-                raise RuntimeError('%s not in %s' % (
-                    get_enc_path(), path))
+        def arg_path():
+            logging.basicConfig(
+                format='%(asctime)s (%(levelname)s) %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+            [enc_path_name, pass_key] = argv[1:3]
+            PathExt = mix_path_utils(Path, my_popen,
+                                     environ[pass_key], my_chmod)
+            return PathExt(enc_path_name)
 
-        if '--dry-run' in argv:
-            mk_fops = partial(Fops.make, chk_path=chk_path, chmod=mock_chmod)
-            mk_decrypt = partial(Decrypt.make, popen=MockPopen,
-                                 chk_path=chk_path)
-        else:
-            mk_fops = partial(Fops.make, chk_path=chk_path, chmod=chmod)
-            mk_decrypt = partial(Decrypt.make, popen=Popen, chk_path=chk_path)
-
-        main(mk_decrypt, mk_fops, get_enc_path, walk, getpass,
-             isfile, abspath, filter_prefix)
+        main(arg_path)
     _tcb()
